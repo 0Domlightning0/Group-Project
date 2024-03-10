fff46eebb0cef549f7a048e4069e1c4a9ae0dae1
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2024-03-10 12:24:44 -0700

    Create Admin.cpp

---
+++ Admin/Admin.cpp
@@ -0,0 +1,59 @@
+#include "Admin.h"
+
+#include "BankUser.h"
+
+Admin::Admin() {
+	adminAccess = false;
+};
+
+Admin::Admin(string fi, string la, string da, string pa) : BankUser(fi,la,da,pa) {
+};
+
+Admin::~Admin() {};
+
+// Creates a new Customer; Datatype will be Customer
+void Admin::regClient() {
+	// return Customer(blah, blah)
+};
+
+// Must input a password to access admin functions
+bool Admin::login(string p) {
+	if (p == get_password()) {
+		cout << "Welcome " << get_firstname() << endl;
+		adminAccess = true;
+		return true;
+	}
+	else {
+		cout << "Incorrect Password "<< endl;
+		return false;
+		adminAccess = false;
+	}
+};
+
+// Shows all the data of the customers
+void Admin::viewCustomers() {
+	if (adminAccess == true) {
+
+	}
+};
+
+// Shows all transactions made on the program, uses transaction class
+void Admin::viewTransactions() {
+	if (adminAccess == true) {
+
+	}
+
+};
+
+// Shows all requests made on the program, uses requests class
+void Admin::viewRequests() {
+	if (adminAccess == true) {
+
+	}
+
+};
+
+// exits the account
+void Admin::logout() {
+
+};
