761c3fd2c209cb6f75f2d4688fbbe585a9bfe2b1
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2024-03-10 12:24:22 -0700

    Create Admin.h

---
+++ Admin/Admin.h
@@ -0,0 +1,50 @@
+// Header File Admin
+
+#pragma once
+
+#include "BankUser.h"
+
+#include <iostream>
+
+using namespace std;
+
+// Connects to Customer, Requests, Transactions
+
+// Parent: BankUser
+
+// Children: None
+
+
+
+
+class Admin : public BankUser
+{
+private:
+	bool adminAccess = false;
+public:
+
+	Admin();
+
+	Admin(string fi, string la, string da, string pa);
+
+	~Admin();
+
+	// Creates a new Customer 
+	void regClient();
+
+	// Must input a password to access admin functions
+	bool login(string p);
+
+	// Shows all the data of the customers
+	void viewCustomers();
+
+	// Shows all transactions made on the program, uses transaction class
+	void viewTransactions();
+
+	// Shows all requests made on the program, uses requests class
+	void viewRequests();
+
+	// exits the account
+	void logout();
+};
+
