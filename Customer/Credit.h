b4c72b90cc2bf034b52f2165d7146eda1a74bfe0
LQWERTY00 <107813022+LQWERTY00@users.noreply.github.com>
2024-03-21 11:51:07 -0400

    Create Credit.h

---
+++ Customer/Credit.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Credit
+{
+private:
+    bool m_lockCard; // Sets whether card is locked or unlocked
+    int m_creditScore;  // Checks credit score
+    double m_overdraw; // implement later...
+
+public:
+    Credit();
+    Credit(bool LC, int score);
+
+    bool getLockCard() { return m_lockCard; 
+    int getCreditScore() { return m_creditScore; }
+    void setLockCard(bool LC) { m_lockCard = LC; }
+    void setCreditScore(int score) { m_creditScore = score; }
+
+    void lockCard(bool LC);
+    void unlockCard(bool LC);
+    void CreditScore(int score);
+};
