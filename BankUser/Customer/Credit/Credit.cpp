bc896977b027d07f95df24abfd2084a3f8f2020f
LQWERTY00 <107813022+LQWERTY00@users.noreply.github.com>
2024-03-21 12:07:36 -0400

    Update Credit.cpp

--- BankUser/Customer/Credit/Credit.cpp
+++ BankUser/Customer/Credit/Credit.cpp
@@ -6,7 +6,6 @@ using namespace std;
 Credit::Credit() {
     m_lockCard = false;
     m_creditScore = 0;
-
 }
 
 Credit::Credit(bool LC, int score) {
@@ -14,18 +13,21 @@ Credit::Credit(bool LC, int score) {
     m_creditScore = score;
 }
 
+// Locks/Freezes the account
 void Credit::lockCard(bool LC) {
     LC = true;
     m_lockCard = LC;
     cout << "Your card is successfully locked.\n";
 }
 
+// Unlocks/Unfreezes the account
 void Credit::unlockCard(bool LC) {
     LC = false;
     m_lockCard = LC;
     cout << "Your card is successfully unlocked.\n";
 }
 
+// Displays user's credit score
 void Credit::CreditScore(int score) {
     cout << "Credit Score: " << m_creditScore << endl;
 }
