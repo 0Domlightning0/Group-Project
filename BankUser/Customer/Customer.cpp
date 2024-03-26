ab25412f856cf60d2ae1401f68418a31b4b540e7
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2024-03-26 08:17:13 -0700

    Update Customer.cpp

--- BankUser/Customer/Customer.cpp
+++ BankUser/Customer/Customer.cpp
@@ -1,1 +1,12 @@
 #include "Customer.h"
+
+#include "BankUser.h"
+
+Customer::Customer() {};
+
+Customer::Customer(string fi, string la, string da, string pa, string ag) : BankUser(fi,la,pa,da,ag)
+{
+
+};
+
+Customer::~Customer() {};
