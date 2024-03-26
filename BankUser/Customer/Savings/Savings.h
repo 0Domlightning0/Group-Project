06b0339c35cc25e844841b29bd4fd9cbc4c4bdd8
LQWERTY00 <107813022+LQWERTY00@users.noreply.github.com>
2024-03-26 11:21:23 -0400

    Update Savings.h

--- BankUser/Customer/Savings/Savings.h
+++ BankUser/Customer/Savings/Savings.h
@@ -1,1 +1,31 @@
-.....
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Savings
+{
+private:
+	string m_displaySavings;
+	double m_interest;
+	string m_lockCard;
+	double m_limit;
+
+public:
+	Savings();
+	Savings(double Interest, string Savings, string LockCard, double Limit);
+
+	string getSavings() { return m_displaySavings; }
+	double getInterest() { return m_interest; }
+	string getLockCard() { return m_lockCard; }
+	double getLimit() { return m_limit; }
+
+	void setSavings(string Savings) { m_displaySavings = Savings; }
+	void setInterest(double Interest) { m_lockCard = Interest; }
+	void setLockCard(string LockCard) { m_lockCard = LockCard; }
+	void setLimit(double Limit) { m_limit = Limit; }
+
+// Functions
+	void displaySavings();
+	void Interest();
+	void accountLimit();
+};
