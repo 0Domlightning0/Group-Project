ddba5de78f75570ce516315995632ffcca586c2f
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2024-03-26 08:21:08 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,18 +3,26 @@
 
 // Libraries
 #include <iostream>
+#include <fstream>
 
 // Files / Classes to include 
 #include "BankUser.h"
 #include "Admin.h"
+#include "Customer.h"
+#include "CustomerSettings.h"
+
 
 
 using namespace std;
 
 int main()
 {
+    ofstream outfile;
+
+    outfile.open("CustomerAccounts");
+
     // Testing Area
-    BankUser User("Dominik", "Wrobelski", "06/03/2024", "Password123");
+    BankUser User("Dominik", "Wrobelski", "06/03/2024", "Password123", "20");
 
     cout << User.get_firstname() << endl;
 
@@ -23,13 +31,20 @@ int main()
     cout << User.get_firstname() << endl;
 
     
-    Admin CEO("Larrry", "Gono", "02/09/1783", "PassPass");
+    CustomerSettings Admin("Larrry", "Gono", "02/09/1783", "PassPass", "20");
 
     string p;
-
+    cout << "Input Password: ";
     cin >> p;
 
-    CEO.login(p);
+    Admin.login(p);
+
+    Customer* list = new Customer[100];
+
+
+    list[0] = Admin.createAccount();
+
+    cout << list[0].get_firstname();
 
     cout << "//////////////////////////////////////END OF TEST CODE//////////////////////////////////////" << endl;
 
