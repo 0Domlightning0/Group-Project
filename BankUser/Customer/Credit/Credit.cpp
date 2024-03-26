c8827d631bc078b89045447e68285c18797d6468
LQWERTY00 <107813022+LQWERTY00@users.noreply.github.com>
2024-03-26 09:44:39 -0400

    Update Credit.cpp

--- BankUser/Customer/Credit/Credit.cpp
+++ BankUser/Customer/Credit/Credit.cpp
@@ -6,28 +6,43 @@ using namespace std;
 Credit::Credit() {
     m_lockCard = false;
     m_creditScore = 0;
+    m_overdraft = false;
 }
 
-Credit::Credit(bool LC, int score) {
+Credit::Credit(bool LC, int score, bool overdraft) {
     m_lockCard = LC;
     m_creditScore = score;
+    m_overdraft = overdraft;
 }
 
 // Locks/Freezes the account
 void Credit::lockCard(bool LC) {
-    LC = true;
+    if (LC == true){
     m_lockCard = LC;
     cout << "Your card is successfully locked.\n";
+    }
 }
 
 // Unlocks/Unfreezes the account
 void Credit::unlockCard(bool LC) {
-    LC = false;
+  if (LC == false){
     m_lockCard = LC;
     cout << "Your card is successfully unlocked.\n";
+  }
 }
 
 // Displays user's credit score
 void Credit::CreditScore(int score) {
     cout << "Credit Score: " << m_creditScore << endl;
+}
+
+// Displays overdraft status
+void Credit::Overdraft(bool overdraw) {
+  if (m_overdraft == false){
+    cout << "Your account is not overdrawn.\n";
+  }
+  if (m_overdraft == true){
+    cout << "Your account is overdrawn.\n";
+    cout << "You will be charged a $25 fee for every overdraft.\n";
+  }
 }
