e4ac75170be53aac203ae152f1a36b58ab78802a
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2024-03-26 08:17:29 -0700

    Update Customer.h

--- BankUser/Customer/Customer.h
+++ BankUser/Customer/Customer.h
@@ -3,6 +3,14 @@
 class Customer :
     public BankUser
 {
+private:
+    int ID = 0;
 
+public:
+    Customer();
+
+    Customer(string fi, string la, string da, string pa, string ag);
+
+    ~Customer();
 };
 
