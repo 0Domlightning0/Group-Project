0ba41ef11b0d36539711e9b95bec74c6cd651883
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2024-03-26 08:15:28 -0700

    Update CustomerSettings.h

--- Admin/CustomerSettings/CustomerSettings.h
+++ Admin/CustomerSettings/CustomerSettings.h
@@ -1,8 +1,28 @@
 #pragma once
 #include "Admin.h"
+#include "Customer.h"
 class CustomerSettings :
-    public Admin
+    public Admin, public Customer
 {
-};
+private:
 
+public:
+    CustomerSettings();
+
+    CustomerSettings(string fi, string la, string da, string pa, string ag);
+
+    ~CustomerSettings();
+
+    bool login(string p);
+
+    // Changing Account Information
+    Customer changeAccount();
+
+    // Creating a Customer Account, creates a new customer object
+    Customer createAccount();
+
+    // Flagging Account
+    void flagAccount();
+
+};
 
