10638e7899c2c501a640abcf10c8a73c4f82ff11
SartFniffer <162068872+SartFniffer@users.noreply.github.com>
2024-03-26 11:15:02 -0400

    Update Transactions.h

--- BankUser/Customer/Transactions/Transactions.h
+++ BankUser/Customer/Transactions/Transactions.h
@@ -1,1 +1,39 @@
+#pragma once
+#include "Customer.h"
+class Transactions :
+    public Customer
+{
+protected:
+    double m_balance;
+    int m_numDep;
+    int m_numWith;
+public:
+    // Default Constructor
+    Transactions();
+
+    // Overloaded Constructor
+    Transactions(double balance, int numDep, int numWith);
+
+    // Deconstructor
+    ~Transactions();
+
+    // Getters
+    double getBalance() { return m_balance; }
+    int getnumDep() { return m_numDep; }
+    int getnumWith() { return m_numWith; }
+
+    // Setters
+    void setBalance(double balance) { m_balance = balance; }
+    void setnumDep(int numDep) { m_numDep = numDep; }
+    void setnumWith(int numWith) { m_numWith = numWith; }
+
+    // Deposit function that allows a user to add money to an account
+    virtual void deposit(double amount);
+
+    // Withdraw function that allows a user to take money out of an account
+    virtual void withdraw(double amount);
+
+
+};
+
 
