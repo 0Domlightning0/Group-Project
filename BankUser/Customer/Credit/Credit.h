0898ad515cfd05a4a02b52fe05b82d816380ffd8
LQWERTY00 <107813022+LQWERTY00@users.noreply.github.com>
2024-03-26 09:41:48 -0400

    Update Credit.h

--- BankUser/Customer/Credit/Credit.h
+++ BankUser/Customer/Credit/Credit.h
@@ -8,20 +8,24 @@ class Credit : public Customer
 private:
     bool m_lockCard;      
     int m_creditScore;
-    double m_overdraw;
+    bool m_overdraft;
 
 public:
     Credit();
-    Credit(bool LC, int score);
+    Credit(bool LC, int score, bool overdraft);
 
 // Getters and setters
-    bool getLockCard() { return m_lockCard; 
+    bool getLockCard() { return m_lockCard; } 
     int getCreditScore() { return m_creditScore; }
+    bool getOverdraft() { return m_overdraft; }
+
     void setLockCard(bool LC) { m_lockCard = LC; }
     void setCreditScore(int score) { m_creditScore = score; }
+    void setOverdraft(bool overdraft) { m_overdraft = overdraft; }
 
 // Functions
-    void lockCard(bool LC);        // Lock or freeze credit card
-    void unlockCard(bool LC);      // Unfreeze account, needs permission
-    void CreditScore(int score);   // Checks credit score
+    void lockCard(bool LC);         // Lock or freeze credit card
+    void unlockCard(bool LC);       // Unfreeze account, needs permission
+    void CreditScore(int score);    // Checks credit score
+    void Overdraft(bool overdraft); // Checks overdraft
 };
