cd9d24938f3795bd2289faafff0ee0af56d350ba
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2024-03-26 18:15:57 -0700

    Update CustomerSettings.cpp

--- Admin/CustomerSettings/CustomerSettings.cpp
+++ Admin/CustomerSettings/CustomerSettings.cpp
@@ -1,18 +1,24 @@
 #include "CustomerSettings.h"
 
+
+#include <fstream>
+
 using namespace std;
 
 CustomerSettings::CustomerSettings() {};
 
 CustomerSettings::~CustomerSettings() {};
 
-CustomerSettings::CustomerSettings(string fi, string la, string da, string pa, string ag) : Admin(fi, la, da, pa, ag) {
+CustomerSettings::CustomerSettings(string fi, string la, string da, string pa, string ag, int id) : Admin(fi, la, da, pa, ag,id) {
 };
 
 bool CustomerSettings::login(string p) {
+
 	if (Admin::login(p) == true) {
+
 		adminAccess = true;
 	}
+
 	return Admin::login(p);
 }
 
@@ -21,28 +27,47 @@ Customer CustomerSettings::changeAccount() {
 	return Customer();
 };
 
+
+
 // Creating a Customer Account, creates a new customer object
-Customer CustomerSettings::createAccount() {
+Customer CustomerSettings::createAccount(ofstream& outfile) {
 
 	if (adminAccess == true) {
+		// Customer Variables
 		string firstname;
 		string lastname;
+		string date;
 		string age;
+		string password;
+		int ID;
 
+		// Input
 		cout << "What is the users first name?";
 		cin >> firstname;
 		cout << "What is the users last name?";
 		cin >> lastname;
+		cout << "What is the date of creation?";
+		cin >> date;
 		cout << "What is the users age?";
 		cin >> age;
+		cout << "What is the users password?";
+		cin >> password;
+		cout << "What is the users ID?";
+		cin >> ID;
 
-		return Customer(firstname, lastname, age, "wsds", "sdsd");
+
+
+		outfile << firstname<< " " << lastname << " "<< date << " " << age << " " << password << " " << ID <<  endl;
+		outfile.flush();
+		return Customer(firstname, lastname, date, password, age, ID);
 	}
 	else {
 		cout << "you do not have access";
 		return Customer();
 	}
 };
+
+
 
 // Flagging Account
 void CustomerSettings::flagAccount() {};
