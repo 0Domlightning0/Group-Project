4ea8a27b073159353bd337f6c58ab6e866bc4a0b
LQWERTY00 <107813022+LQWERTY00@users.noreply.github.com>
2024-03-28 10:57:27 -0400

    Update Savings.cpp

--- BankUser/Customer/Savings/Savings.cpp
+++ BankUser/Customer/Savings/Savings.cpp
@@ -3,26 +3,47 @@
 #include <string>
 using namespace std;
 
-Savings::Savings(){
-	m_displaySavings = "";
-	m_interest = 0.0;
-	m_lockCard = "";
-	m_limit = 3000.0;
+Savings::Savings() {
+  m_savingsAmount = 0.0;
+  m_displaySavings = "";
+  m_interest = 3.5;
+  m_lockCard = false;
+  m_limit = 3000.0;
 }
 
-Savings::Savings(double Interest, string Savings, string LockCard, double Limit){
-	m_displaySavings = Savings;
-	m_interest = Interest;
-	m_lockCard = LockCard;
-	m_limit = Limit;
+Savings::Savings(string Display, double Interest, double Savings, bool LockCard, double Limit) {
+  m_savingsAmount = Savings;
+  m_displaySavings = Display;
+  m_interest = Interest;
+  m_lockCard = LockCard;
+  m_limit = Limit;
 }
-
-void Savings::displaySavings(){}
-
-void Savings::Interest(){}
-
-void Savings::accountLimit(){
-	if ([AMOUNT] > 3000.0) {}
-
-	else if ([AMOUNT] < 3000.0){}
+// Displays the user's savings amount
+void Savings::displaySavings() {
+  cout << "Your current saving amount is: $" << m_displaySavings << endl;
+  }
+// Request to lock account
+void Savings::requestLock(){
+  m_lockCard = true;
+  cout << "Your account is now locked.\n";
 }
+// Request to unlock account
+void Savings::requestUnlock(){
+  m_lockCard = false;
+  cout << "Your account is now unlocked.\n";
+}
+// Displays the user's interest rate
+// Outputs the new savings amount after interest
+void Savings::Interest() {
+  cout << "The interest rate for your savings account is: " << m_interest << "%" << endl;
+  m_savingsAmount * m_interest = m_savingsAmount;
+}
+// Sets a limit to your savings account
+void Savings::accountLimit(double Limit) {
+  m_limit = Limit;
+  cout << "Your card limit has been set to: $" << m_limit << endl;
+  if (m_savingsAmount += m_savingsAmount > 3000.0) {
+  }
+
+  else if (m_savingsAmount += m+savingsAmount < 3000.0) {
+  }
