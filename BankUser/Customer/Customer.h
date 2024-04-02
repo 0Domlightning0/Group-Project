25cdf07bd716c19a13a8faa38a620620661b909e
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2024-04-01 22:29:50 -0700

    Update Customer.h

--- BankUser/Customer/Customer.h
+++ BankUser/Customer/Customer.h
@@ -4,12 +4,21 @@ class Customer :
     public BankUser
 {
 private:
-
+    double balance = 0;
+    bool flagged = false;
 public:
     Customer();
 
-    Customer(string fi, string la, string da, string pa, string ag, int id);
+    Customer(string fi, string la, string da, string pa, string ag, int id, double ba, bool fl);
 
     ~Customer();
+
+    // getters 
+    double get_balance() { return balance; };
+    bool get_flagged() { return flagged; }
+
+    //setters
+    void set_balance(double ba) { balance = ba; };
+    void set_flagged(bool fl) { flagged = fl; };
 };
 
