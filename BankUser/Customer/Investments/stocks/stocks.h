218c0652a440340bb742171f12e5f40ad963603f
Beggsy27 <157051208+Beggsy27@users.noreply.github.com>
2024-04-01 23:16:08 -0400

    Update stocks.h

--- BankUser/Customer/Investments/stocks/stocks.h
+++ BankUser/Customer/Investments/stocks/stocks.h
@@ -1,7 +1,20 @@
-#pragma once
+#include <ctime>
+
+using namespace std;
+
 #include "investments.h"
 class stocks :  public investments {
-	stocks();
-	stocks(double inv, double bal);
+public:
+    stocks();
+	stocks(double rate);
 	~stocks();
+
+    void updateRate() {
+        // Randomize rate for Stocks between -12% and 15%
+        rate = -0.12 + static_cast<double>(rand()) / RAND_MAX * (0.15 - (-0.12));
+    }
+
+    double calculateReturn(double amount) {
+        return amount * (1 + rate);
+    }
 };
