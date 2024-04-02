ef377022a88c556563a1c5ca53d01202ac8dda9b
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2024-04-01 22:32:03 -0700

    Update Customer.cpp

--- BankUser/Customer/Customer.cpp
+++ BankUser/Customer/Customer.cpp
@@ -4,9 +4,10 @@
 
 Customer::Customer() {};
 
-Customer::Customer(string fi, string la, string da, string pa, string ag, int id) : BankUser(fi,la,pa,da,ag,id)
+Customer::Customer(string fi, string la, string da, string pa, string ag, int id, double ba, bool fl) : BankUser(fi,la,pa,da,ag,id)
 {
-
+	balance = ba;
+	flagged = fl;
 };
 
 Customer::~Customer() {};
