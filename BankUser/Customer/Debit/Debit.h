cf97c1f98d192bdf3f13428cb7486b7f908ad660
LQWERTY00 <107813022+LQWERTY00@users.noreply.github.com>
2024-04-02 02:14:54 -0400

    Update Debit.h

--- BankUser/Customer/Debit/Debit.h
+++ BankUser/Customer/Debit/Debit.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include "Customer.h"
 using namespace std;
-class Debit //: public Customer
+class Debit : public Customer
 {
 private:
     double m_debitAmount;
@@ -14,7 +15,7 @@ public:
     Debit();
     Debit(double DebitAmount, string Debit, string LockCard, bool Overdraft);
 
-// Getters and setters
+    // Getters and setters
     double getDebitAmount() { return m_debitAmount; }
     string getDebit() { return m_displayDebit; }
     string getLockCard() { return m_lockCard; }
@@ -25,7 +26,7 @@ public:
     void setLockCard(string LC) { m_lockCard = LC; }
     void setOverdraft(bool overdraft) { m_overdraft = overdraft; }
 
-// Functions
+    // Functions
     void displayDebit();  // Outputs vslue in account
     void lockCard();      // Lock or freeze credit card
     void unlockCard();    // Unfreeze account, need permission
