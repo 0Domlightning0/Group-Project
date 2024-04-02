fa837f6ea523bdbca3255e3abac5041cb470faa4
Beggsy27 <157051208+Beggsy27@users.noreply.github.com>
2024-04-01 23:14:52 -0400

    Update TFSA.h

--- BankUser/Customer/Investments/TFSA/TFSA.h
+++ BankUser/Customer/Investments/TFSA/TFSA.h
@@ -1,8 +1,23 @@
 #pragma once
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "investments.h"
+
+using namespace std;
+
 class TFSA :  public investments {
 public:
-	TFSA();
-	TFSA(double inv, double bal);
+    TFSA();
+    TFSA(double rate);
 	~TFSA();
+
+    void updateRate() {
+        // Randomize rate for TFSA between -6% and 10%
+        rate = -0.06 + static_cast<double>(rand()) / RAND_MAX * (0.10 - (-0.06));
+    }
+
+    double calculateReturn(double amount) {
+        return amount * (1 + rate);
+    }
 };
