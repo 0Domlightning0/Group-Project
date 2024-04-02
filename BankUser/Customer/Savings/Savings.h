ba5711924b8ddd3c714955ff2ce9f5079de6a2c0
LQWERTY00 <107813022+LQWERTY00@users.noreply.github.com>
2024-04-02 02:16:04 -0400

    Update Savings.h

--- BankUser/Customer/Savings/Savings.h
+++ BankUser/Customer/Savings/Savings.h
@@ -1,37 +1,38 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include "Customer.h"
 using namespace std;
 class Savings : public Customer
 {
 private: // Variables
-  double m_savingsAmount;
-  string m_displaySavings;
-  double m_interest;
-  bool m_lockCard;
-  double m_limit;
+	double m_savingsAmount;
+	double m_displaySavings;
+	double m_interest;
+	bool m_lockCard;
+	double m_limit;
 
 public:
-  Savings();
-  Savings(string Display, double Interest, double Savings, bool LockCard, double Limit);
+	Savings();
+	Savings(double Display, double Interest, double Amount, bool LockCard, double Limit);
 
 // Getters and Setters
-  double getSavingsAmount() { return m_savingsAmount; }
-  string getSavings() { return m_displaySavings; }
-  double getInterest() { return m_interest; }
-  bool getLockCard() { return m_lockCard; }
-  double getLimit() { return m_limit; }
+	double getSavingsAmount() { return m_savingsAmount; }
+	double getSavings() { return m_displaySavings; }
+	double getInterest() { return m_interest; }
+	bool getLockCard() { return m_lockCard; }
+	double getLimit() { return m_limit; }
 
-  void setSavingsAmount(double Savings) { m_savingsAmount = Savings; }
-  void setSavings(string Display) { m_displaySavings = Display; }
-  void setInterest(double Interest) { m_lockCard = Interest; }
-  void setLockCard(bool LockCard) { m_lockCard = LockCard; }
-  void setLimit(double Limit) { m_limit = Limit; }
+	void setSavingsAmount(double Savings) { m_savingsAmount = Savings; }
+	void setSavings(double Display) { m_displaySavings = Display; }
+	void setInterest(double Interest) { m_lockCard = Interest; }
+	void setLockCard(bool LockCard) { m_lockCard = LockCard; }
+	void setLimit(double Limit) { m_limit = Limit; }
 
-  // Functions
-  void displaySavings();
-  void requestLock();
-  void requestUnlock();
-  void Interest();
-  void accountLimit(double Limit);
+// Functions
+	void displaySavings();
+	void requestLock();
+	void requestUnlock();
+	void Interest(double Interest);
+	void accountLimit(double Limit);
 };
