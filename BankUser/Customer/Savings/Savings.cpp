42e91e3590fd4478f1ab7f2476e8f040a98042dd
LQWERTY00 <107813022+LQWERTY00@users.noreply.github.com>
2024-04-02 02:16:34 -0400

    Update Savings.cpp

--- BankUser/Customer/Savings/Savings.cpp
+++ BankUser/Customer/Savings/Savings.cpp
@@ -4,47 +4,60 @@
 using namespace std;
 
 Savings::Savings() {
-  m_savingsAmount = 0.0;
-  m_displaySavings = "";
-  m_interest = 3.5;
-  m_lockCard = false;
-  m_limit = 3000.0;
+	m_savingsAmount = 0.0;
+	m_displaySavings = 0.0;
+	m_interest = 0.0;
+	m_lockCard = false;
+	m_limit = 0.0;
 }
 
-Savings::Savings(string Display, double Interest, double Savings, bool LockCard, double Limit) {
-  m_savingsAmount = Savings;
-  m_displaySavings = Display;
-  m_interest = Interest;
-  m_lockCard = LockCard;
-  m_limit = Limit;
+Savings::Savings(double Display, double Interest, double Amount, bool LockCard, double Limit) {
+	m_displaySavings = Display;
+	m_savingsAmount = Amount;
+	m_interest = Interest;
+	m_lockCard = LockCard;
+	m_limit = Limit;
 }
+
 // Displays the user's savings amount
 void Savings::displaySavings() {
-  cout << "Your current saving amount is: $" << m_displaySavings << endl;
-  }
+	cout << "Your current savings amount is $" << m_displaySavings << endl;
+}
+
 // Request to lock account
-void Savings::requestLock(){
-  m_lockCard = true;
-  cout << "Your account is now locked.\n";
+void Savings::requestLock() {
+	if (m_lockCard == false) {
+		m_lockCard = true;
+		cout << "Your account is now locked.\n";
+	}
+	else { cout << "ACCOUNT ALREADY LOCKED\n"; }
 }
+
 // Request to unlock account
-void Savings::requestUnlock(){
-  m_lockCard = false;
-  cout << "Your account is now unlocked.\n";
+void Savings::requestUnlock() {
+	if (m_lockCard == true) {
+		m_lockCard = false;
+		cout << "Your account is now unlocked.\n";
+	}
+	else { cout << "ACCOUNT ALREADY UNLOCKD\n"; }
 }
+
 // Displays the user's interest rate
 // Outputs the new savings amount after interest
-void Savings::Interest() {
-  cout << "The interest rate for your savings account is: " << m_interest << "%" << endl;
-  m_savingsAmount * m_interest = m_savingsAmount;
+void Savings::Interest(double Interest) {
+	cout << "The interest rate is " << m_interest << "%" << endl;
+	m_savingsAmount = (m_savingsAmount * m_interest);
 }
+
 // Sets a limit to your savings account
 void Savings::accountLimit(double Limit) {
-  m_limit = Limit;
-  cout << "Your card limit has been set to: $" << m_limit << endl;
-  if (m_savingsAmount += m_savingsAmount > 3000.0) {
-  }
-
-  else if (m_savingsAmount += m+savingsAmount < 3000.0) {
-  }
+	m_limit = Limit;
+	cout << "Your card limit has been set to $" << m_limit << endl;
+	if (m_savingsAmount += m_limit > 3000.0) {
+		m_savingsAmount += m_limit;
+		cout << "Current amount is " << m_savingsAmount << endl;
+	}
+	else if (m_savingsAmount += m_savingsAmount < 3000.0) {
+		cout << "You have reached your limit.\n";
+	}
 }
