c15c45a7bd4bffa4b079d9235ade657d9d1a8fbf
Beggsy27 <157051208+Beggsy27@users.noreply.github.com>
2024-04-01 23:15:32 -0400

    Update GIC.h

--- BankUser/Customer/Investments/GIC/GIC.h
+++ BankUser/Customer/Investments/GIC/GIC.h
@@ -1,8 +1,24 @@
 #pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
 #include "investments.h"
 class GIC :  public investments {
 public:
-	GIC();
-	GIC(double inv, double bal);
+    GIC();
+	GIC(double rate);
 	~GIC();
+
+    void updateRate() {
+        // Randomize rate for GIC between 2% and 5%
+        rate = 0.02 + static_cast<double>(rand()) / RAND_MAX * (0.05 - 0.02);
+    }
+
+    double calculateReturn(double amount) {
+        return amount * (1 + rate);
+    }
 };
