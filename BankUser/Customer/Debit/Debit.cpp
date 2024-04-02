795c494a5fc3ca95b9689140aecca7730cddf25f
LQWERTY00 <107813022+LQWERTY00@users.noreply.github.com>
2024-04-02 02:15:28 -0400

    Update Debit.cpp

--- BankUser/Customer/Debit/Debit.cpp
+++ BankUser/Customer/Debit/Debit.cpp
@@ -9,36 +9,36 @@ Debit::Debit() {
     m_overdraft = false;
 }
 
-Debit::Debit(double DebitAmount, string Debit, string LockCard, bool Overdraft){
+Debit::Debit(double DebitAmount, string Debit, string LockCard, bool Overdraft) {
     m_displayDebit = Debit;
     m_lockCard = LockCard;
     m_overdraft = Overdraft;
 }
 
-void Debit::displayDebit(){
+void Debit::displayDebit() {
     cout << "W";
 }
 
 void Debit::lockCard() {
     if (m_lockCard == "OPEN") {
         m_lockCard = "CLOSED";
-    cout << "Your card is successfully locked.\n";
+        cout << "Your card is successfully locked.\n";
     }
 }
 
 void Debit::unlockCard() {
-  if (m_lockCard == "CLOSED") {
-      m_lockCard = "OPEN";
-    cout << "Your card is successfully unlocked.\n";
-  }
+    if (m_lockCard == "CLOSED") {
+        m_lockCard = "OPEN";
+        cout << "Your card is successfully unlocked.\n";
+    }
 }
 
 void Debit::Overdraft() { // [set a limit for the overdraft]
-  if (m_overdraft == false){
-    cout << "Your account is not overdrawn.\n";
-  }
-  if (m_overdraft == true){
-    cout << "Your account is overdrawn.\n";
-    cout << "You will be charged a $25 fee for every overdraft.\n";
-  }
+    if (m_overdraft == false) {
+        cout << "Your account is not overdrawn.\n";
+    }
+    if (m_overdraft == true) {
+        cout << "Your account is overdrawn.\n";
+        cout << "You will be charged a $25 fee for every overdraft.\n";
+    }
 }
